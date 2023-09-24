9852a45c945cebdd5a62e8304b42a5781c223fc4
H3wastooshort <hacker3000@posteo.org>
2023-09-24 16:50:11 +0200

    fix summing

--- dice_server.py
+++ dice_server.py
@@ -13,13 +13,19 @@ def reset_outcomes(oc,po):
 reset_outcomes(outcomes,possible_outcomes)
 
 def get_max(l):
-    max_x = 0
+    max_x = l[0]
     for x in l:
         if x > max_x:
             max_x = x
     return max_x
+def get_min(l):
+    min_x = l[0]
+    for x in l:
+        if x < min_x:
+            min_x = x
+    return min_x
 
-possible_sum_outcomes = range(1,(get_max(possible_outcomes)*outcomes_per_sum)+1,1)
+possible_sum_outcomes = range(get_min(possible_outcomes)*outcomes_per_sum,(get_max(possible_outcomes)*outcomes_per_sum)+1,1)
 sum_outcomes = {}
 def reset_outcomes(oc,po):
     for x in po:
@@ -39,6 +45,24 @@ def do_plot():
     new_data=False
     
     global stepplot, fig, ax
+    sum_ocv = list(sum_outcomes.values())
+    max_y = get_max(sum_ocv)+1
+    ax.set_ylim(0,max_y);
+    stepplot.set_ydata(sum_ocv)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+async def plot_loop():
+    global plt
+    while True:
+        if new_data:
+            do_plot()
+        plt.gcf().canvas.draw_idle()
+        plt.gcf().canvas.start_event_loop(0.1)
+        await asyncio.sleep(0.1)
+
+def do_outcome_sum():
+    global new_data
     n=0
     for v in outcomes.values():
         n+=v
@@ -50,23 +74,8 @@ def do_plot():
             outcomes[k]=0
         print(o_sum)
         sum_outcomes[o_sum] += 1
+    new_data=True
         
-        #draw plot
-        sum_ocv = list(sum_outcomes.values())
-        max_y = get_max(sum_ocv)+1
-        ax.set_ylim(0,max_y);
-        stepplot.set_ydata(sum_ocv)
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-
-async def plot_loop():
-    global plt
-    while True:
-        if new_data:
-            do_plot()
-        plt.gcf().canvas.draw_idle()
-        plt.gcf().canvas.start_event_loop(0.1)
-        await asyncio.sleep(0.1)
 
 async def add_outcome(req):
     global new_data
@@ -79,8 +88,8 @@ async def add_outcome(req):
     if n not in possible_outcomes:
         return web.Response(status=400,text="unknown outcome")
     outcomes[n] += 1
-    new_data=True
     print(outcomes)
+    do_outcome_sum()
     return web.Response(text="ok")
 
 #setup loop
