7e2d01f3e990366584b6124ed6f12c30ae85c782
H3wastooshort <hacker3000@posteo.org>
2023-09-24 14:41:01 +0200

    plot working

--- dice_server.py
+++ dice_server.py
@@ -1,25 +1,68 @@
+import asyncio
 from aiohttp import web
+from matplotlib import pyplot as plt
 import numpy
 
-new_data_flag=False
-possible_outcomes = [1,2,3,4,5,6]
+possible_outcomes = ['1','2','3','4','5','6']
 outcomes = {}
 for o in possible_outcomes:
     outcomes[o] = 0
 
+#set up plot
+plt.ion()
+plt.show()
+fig,ax = plt.subplots()
+stepplot, = ax.step(outcomes.keys(), outcomes.values(), where='mid')
+
+new_data = True
+
+def do_plot():
+    global new_data
+    new_data=False
+    
+    global stepplot, fig, ax
+    ocv = list(outcomes.values())
+    max_y = 0
+    for y in ocv:
+        if y > max_y:
+            max_y = y+1
+    ax.set_ylim(-1,max_y);
+    stepplot.set_ydata(ocv)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+async def plot_loop():
+    global plt
+    while True:
+        if new_data:
+            do_plot()
+        plt.pause(0.01)
+        await asyncio.sleep(0.1)
+
 async def add_outcome(req):
-    #add stuff
-    new_data_flag=True
+    global new_data
+    dat = await req.text()
+    if dat not in possible_outcomes:
+        return web.Response(status=400,text="unknown outcome")
+    outcomes[dat] += 1
+    new_data=True
+    print(outcomes)
     return web.Response(text="ok")
 
+#setup loop
+loop = asyncio.get_event_loop()
+loop.create_task(plot_loop())
+
 #start webserver
-app = web.Application()
+app = web.Application(loop=loop)
 app.add_routes([
-    web.static('/', "./student"),
+    web.static('/', "student"),
     web.post('/outcome', add_outcome)
 ])
-web.run_app(app)
-
-while True:
-    
-#https://stackoverflow.com/questions/4098131/how-to-update-a-plot-in-matplotlib
+server = loop.create_server(app.make_handler(), '127.0.0.1', 8000)
+print("Server started at http://127.0.0.1:8000")
+loop.run_until_complete(server)
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
--- student/student.js
+++ student/student.js
@@ -9,14 +9,18 @@ possible_outcomes.forEach(o => {
 });
 
 function send_outcome(evt) {
-	evt.srcElement.style.borderColor = "orange";
+	evt.srcElement.style.borderColor = "cyan";
 	fetch("/outcome", {
 		method: "POST",
 		headers: {"Content-Type": "text/plain"},
 		body: evt.srcElement.innerText
 	})
 	.then(r => {
-		evt.srcElement.style.borderColor = r.ok ? "green" : "magenta";
+		evt.srcElement.style.borderColor = r.ok ? "white" : "red";
+		if (!r.ok) r.text().then(t=>{alert("SERVER ERROR:\n" + t)});
 	})
-	.catch(err => {evt.srcElement.style.borderColor = "magenta";});
+	.catch(err => {
+		evt.srcElement.style.borderColor = "red";
+		alert("CLIENT ERROR:\n"+err.message);
+	});
 }
