ff5073d1da80f566ff44c1f9213532773d2784d8
H3wastooshort <hacker3000@posteo.org>
2023-10-05 19:06:44 +0200

    works

--- dice_server.py
+++ dice_server.py
@@ -34,22 +34,20 @@ def normal_dist_area(start,stop):
 
 def do_iter(i):
     #calc
-    bx=[]
-    bw=[]
-    bh=[]
+    bars={}
     bar_width = 1/i
-    for n in range(0,i+1,1):
-        x = n/bar_width
-        bx[n]=x
-        bw[n]=bar_width
-        bh[n]=normal_dist_area(x,x+bar_width)
-
+    for n in range(0,i):
+        x = n*bar_width
+        bars[x]=normal_dist(x+(bar_width/2),0,0.25)# not real integral, should be width-root of integral
+    text="A="+str(bars[0]*bar_width) # not real integral, whatever
     #draw (badly)
     ax.clear()
-    barplot, = ax.bar(bx,bh,bw, align='left',color='blue')
+    ax.bar(x=list(bars.keys()),height=bars.values(),width=bar_width, align='edge',edgecolor='blue',color='lightblue')
+    ax.annotate(text, xy=(bar_width,bars[0]),textcoords='axes fraction',va='top', ha='right',xytext=(0.95,0.95), fontsize='xx-large', arrowprops=dict(facecolor='black', shrink=0.05))
 
-iteration=0
+iteration=1
 while True:
-    do_iter(iteration++)
+    do_iter(iteration)
+    iteration += 1
     plt.gcf().canvas.draw_idle()
     plt.gcf().canvas.start_event_loop(0.5)
