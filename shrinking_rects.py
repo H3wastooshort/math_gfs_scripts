593333fee32ae8d212102eed1758075e52119c86
H3wastooshort <hacker3000@posteo.org>
2023-10-13 23:03:45 +0200

    plot symmetrically

--- shrinking_rects.py
+++ shrinking_rects.py
@@ -3,7 +3,6 @@ from time import sleep
 from matplotlib import pyplot as plt
 
 graph_sigma=0.5
-graph_width=graph_sigma*3
 
 #set up plot
 plt.rc('axes', labelsize='x-large', titlesize='xx-large')
@@ -23,11 +22,12 @@ def plot_done():
     plt.gcf().canvas.draw_idle()
     plt.gcf().canvas.flush_events()
     print("plot completed")
+graph_width=graph_sigma*5
 def draw_bars(n): #something causes errors here, idk what \(°-°)/
     ##calc
     bars={}
     bar_width = graph_width/n
-    for i in range(0,n):
+    for i in range(-n,n):
         x = i * bar_width
         bars[x] = normal_dist_area(x,x+bar_width)
     text="P="+str(normal_dist_area(0,bar_width))
@@ -35,12 +35,12 @@ def draw_bars(n): #something causes errors here, idk what \(°-°)/
     ax.clear()
     ax.bar(x=list(bars.keys()),height=bars.values(),width=bar_width, align='edge',edgecolor='blue',color='lightblue')
     ax.annotate(text, xy=(bar_width,bars[0]),textcoords='axes fraction',va='top', ha='right',xytext=(0.95,0.95), fontsize='xx-large', arrowprops=dict(facecolor='black', shrink=0.05))
-    ax.set_xlim(0,graph_width)
+    ax.set_xlim(-graph_width,graph_width)
     plot_done()
 
 def input_loop():
     while True:
-        inp=input("Enter n of bars: ")
+        inp=input("Enter n/2 of bars: ")
         if inp == 'q':
             quit()
         if inp.isdigit():
