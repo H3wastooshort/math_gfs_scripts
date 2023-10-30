7e666a4a62d99579673426ff532215db567c0cbb
H3wastooshort <hacker3000@posteo.org>
2023-10-30 21:59:04 +0100

    trying to package this

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import py2exe
+
+# ``bundle_files`` option explained:
+# ===================================================
+#
+# The py2exe runtime *can* use extension module by directly importing
+# the from a zip-archive - without the need to unpack them to the file
+# system.  The bundle_files option specifies where the extension modules,
+# the python dll itself, and other needed dlls are put.
+#
+# bundle_files == 3:
+#     Extension modules, the Python dll and other needed dlls are
+#     copied into the directory where the zipfile or the exe/dll files
+#     are created, and loaded in the normal way.
+#
+# bundle_files == 2:
+#     Extension modules are put into the library ziparchive and loaded
+#     from it directly.
+#     The Python dll and any other needed dlls are copied into the
+#     directory where the zipfile or the exe/dll files are created,
+#     and loaded in the normal way.
+#
+# bundle_files == 1:
+#     Extension modules and the Python dll are put into the zipfile or
+#     the exe/dll files, and everything is loaded without unpacking to
+#     the file system.  This does not work for some dlls, so use with
+#     caution.
+#
+# bundle_files == 0:
+#     Extension modules, the Python dll, and other needed dlls are put
+#     into the zipfile or the exe/dll files, and everything is loaded
+#     without unpacking to the file system.  This does not work for
+#     some dlls, so use with caution.
+
+
+py2exe_options = dict(
+    packages = ['scipy','matplotlib','asyncio'],
+    optimize=0,
+    compressed=True,
+    bundle_files=0,
+    dist_dir='dist'
+)
+
+
+setup(
+    name="binom_graph",
+    console=["binom_graph.py"],
+    options={"py2exe": py2exe_options},
+)
+
